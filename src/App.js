b2203f800bda4dd326ce40a934da3ffb7b55eed8
Davi Camarinha <davicamarinha@gmail.com>
2020-05-20 17:54:09 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import CircularProgressBar from './components/Circular-progress-bar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CircularProgressBar goal={100} balance={90} color="#ff0000"/>
     </div>
   );
 }
